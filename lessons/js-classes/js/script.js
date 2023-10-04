8a0fb1c2339870d42cb4569252da7f426d964d57
ianmacdonald <ianpetercreative@gmail.com>
2023-10-04 07:21:56 -0600

    add starting code

---
+++ lessons/js-classes/js/script.js
@@ -0,0 +1,15 @@
+// app's state (variables) 
+let game;
+
+// cached element references 
+const boardEl = document.getElementById('board');
+const msgEl = document.getElementById('message');
+
+// classes
+
+// functions 
+initialize();
+
+function initialize() {
+
+}
