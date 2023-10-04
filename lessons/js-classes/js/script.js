684fdf47ef9ee5c5786d0ad4db5c49515921839e
ianmacdonald <ianpetercreative@gmail.com>
2023-10-04 13:32:53 -0600

    complete js classes lesson 1

--- lessons/js-classes/js/script.js
+++ lessons/js-classes/js/script.js
@@ -6,10 +6,48 @@ const boardEl = document.getElementById('board');
 const msgEl = document.getElementById('message');
 
 // classes
+class TicTacToeGame {
+    constructor(boardElement, messageElement) {
+        this.boardElement = boardElement;
+        this.messageElement = messageElement;
+        this.squareEls = [...boardElement.querySelectorAll('div')]
+    }
+    
+    static winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    
+    play() {
+        this.turn = 1;
+        this.winner = null;
+        // this.squares = ???; 
+        this.render();
+    }
+
+    render() {
+        console.log("Render game...")
+    }
+
+    toString() {
+        return `Tic-Tac-Toe / winner -> ${this.winner}`
+    }
+
+    static about() {
+        console.log("I'm the TicTacToeGame class!")
+    };
+}
 
 // functions 
 initialize();
 
 function initialize() {
+    game = new TicTacToeGame(boardEl, msgEl);
 
 }
