675e75d5305f55790123ed6c9d28fa9d52d4d66f
ianmacdonald <ianpetercreative@gmail.com>
2023-10-11 18:19:46 -0600

    add multi-jump functionality

--- projects/unit01-project/js/script.js
+++ projects/unit01-project/js/script.js
@@ -6,12 +6,6 @@ const gameBoardEl = document.getElementById('game-board');
 
 const rowEls = [...gameBoardEl.querySelectorAll('.row')];
 
-// const squareEls = [...document.querySelectorAll('.square')];
-
-// const redPucks = [...document.querySelectorAll('.red-puck'), ...document.querySelectorAll('.red-king')];
-
-// const blackPucks = [...document.querySelectorAll('.black-puck'), ...document.querySelectorAll('.black-king')];
-
 const playerTurnMsg = document.getElementById('player-turn');
 
 
@@ -31,32 +25,32 @@ const playerTurnMsg = document.getElementById('player-turn');
 //   [1, null, 1, null, 1, null, 1, null],
 // ];
 
-// checking king functionality 
-// const gameBoard = [
-//   // I'll need to investigate further the values of these places on the board to deny movement into the unplayable squares 
-//   [null, 0, null, 0, null, 0, null, 0],
-//   [0, null, 0, null, 1, null, 0, null],
-//   [null, -2, null, 0, null, 0, null, 0],
-//   [0, null, 0, null, 0, null, 0, null],
-//   [null, 2, null, 0, null, , null, 0],
-//   [0, null, 0, null, 0, null, 0, null],
-//   [null, 0, null, 0, null, -1, null, 0],
-//   [0, null, 0, null, 0, null, 0, null],
-// ];
-
-
-// checking multijump functionality
+// checking win functionality 
 const gameBoard = [
   // I'll need to investigate further the values of these places on the board to deny movement into the unplayable squares 
   [null, 0, null, 0, null, 0, null, 0],
   [0, null, 0, null, 0, null, 0, null],
-  [null, 0, null, -1, null, 0, null, 0],
+  [null, 0, null, 0, null, 0, null, 0],
+  [0, null, -1, null, 0, null, 0, null],
+  [null, 2, null, 0, null, , null, 0],
   [0, null, 0, null, 0, null, 0, null],
-  [null, 0, null, -1, null, , null, 0],
+  [null, 0, null, 0, null, 0, null, 0],
   [0, null, 0, null, 0, null, 0, null],
-  [null, 0, null, -1, null, 0, null, 0],
-  [0, null, 0, null, 1, null, 0, null],
 ];
+
+
+// checking multijump functionality
+// const gameBoard = [
+//   // I'll need to investigate further the values of these places on the board to deny movement into the unplayable squares 
+//   [null, 0, null, 0, null, 0, null, 0],
+//   [0, null, 0, null, -1, null, 0, null],
+//   [null, 0, null, 0, null, 0, null, 0],
+//   [0, null, 1, null, 0, null, 0, null],
+//   [null, 0, null, 1, null, , null, 0],
+//   [0, null, 0, null, 0, null, 0, null],
+//   [null, 0, null, 1, null, -1, null, 0],
+//   [0, null, 0, null, 0, null, 0, null],
+// ];
 // how to track the king [2, -2]
 // whose piece is this? positive or negative 
 
@@ -64,20 +58,21 @@ const playerOne = [1, 2];
 const playerTwo = [-1, -2];
 
 let playerTurn = 1;
+let winner = null;
 
 let playerPucks = [];
 let playerKings = [];
 
-let winner = null;
-
 let movablePucks = [];
 let jumpsAvailable = [];
-
+let multiJumps = [];
 let destinations = [];
 
 let selectedPuck = null;
 let selectedPuckRow = null;
 let selectedPuckCol = null;
+let moveToRow = null;
+let moveToCol = null;
 
 
 /*----- event listeners -----*/
@@ -100,33 +95,31 @@ gameBoardEl.addEventListener('click', function (evt) {
     selectedPuck = evt.target;
     selectedPuckRow = rowIdx
     selectedPuckCol = colIdx
-    console.log(selectedPuck)
   } else if (clickedEl.classList.contains('destination')) {
-    const newRow = parseInt(clickedEl.dataset.row);
-    const newCol = parseInt(clickedEl.dataset.col);
-    console.log(newRow, newCol, selectedPuckRow, selectedPuckCol)
-    if (jumpsAvailable.length > 0) {
-      clearDestinationHighlights(); 
-      const jumpRow = destinations[0].removePuckFromRow;
-      const jumpCol = destinations[0].removePuckFromCol
-      jumpPuck(selectedPuck, selectedPuckRow, selectedPuckCol, newRow, newCol, jumpRow, jumpCol)
+    moveToRow = parseInt(clickedEl.dataset.row);
+    moveToCol = parseInt(clickedEl.dataset.col);
+    if (jumpsAvailable.length > 0 || multiJumps.length > 0) {
+      clearDestinationHighlights();
+      jumpPuck(selectedPuck, selectedPuckRow, selectedPuckCol, moveToRow, moveToCol)
+      // render();
+
       movablePucks = [];
-      possibleJumps = [];  
-      jumpsAvailable = []; 
+      possibleJumps = [];
+      jumpsAvailable = [];
 
       checkForKings();
-      changePlayer();
+      // changePlayer();
       checkForWinner();
     } else {
 
-  
+
       clearDestinationHighlights();
       destinations = [];
 
-      movePuck(selectedPuck, selectedPuckRow, selectedPuckCol, newRow, newCol);
+      movePuck(selectedPuck, selectedPuckRow, selectedPuckCol, moveToRow, moveToCol);
       movablePucks = [];
-  
-  
+
+
       checkForKings();
       changePlayer();
       checkForWinner();
@@ -141,7 +134,6 @@ gameBoardEl.addEventListener('click', function (evt) {
     selectedPuckRow = null;
     selectedPuckCol = null;
     movablePucks = [];
-    // console.log(selectedPuck)
 
     // Implement logic to deselect the selected puck (if any).
     // You should clear any selected puck variable here.
@@ -150,11 +142,7 @@ gameBoardEl.addEventListener('click', function (evt) {
     render();
   }
 
-  // console.log("This should only log if I click on a puck")
 
-  // console.log(destinations)
-
-  // console.log(playerTurn, rowIdx, colIdx)
 
   // checkForMoves(playerTurn, rowIdx, colIdx);
 })
@@ -194,7 +182,13 @@ function render() {
   }
   // display player turn 
   // checkForWinner(); 
-  playerTurnMsg.textContent = `Player ${playerTurn}'s Turn`
+  if (playerTurn === 1) {
+    playerTurnMsg.textContent = `Red's Turn`
+
+  } else if (playerTurn === -1) {
+    playerTurnMsg.textContent = `Black's Turn`
+
+  }
   checkForMoves(playerTurn);
 }
 
@@ -206,18 +200,24 @@ function changePlayer() {
 
 // invoke remaining pucks and legal moves to determine a winner 
 function checkForWinner() {
-  playerPucks = getAllPlayerPucks(playerTurn); 
+  playerPucks = getAllPlayerPucks(playerTurn);
   playerKings = getAllPlayerKings(playerTurn)
   // once a player is out of chips
-  
+
   const currentPlayerPucks = playerPucks.length + playerKings.length
-  console.log(currentPlayerPucks)
-  // console.log(currentPlayerPucks)
   if (currentPlayerPucks === 0) {
     winner = playerTurn * -1;
-    playerTurnMsg.textContent = `Player ${winner} wins!`
+    if (winner === 1) {
+      playerTurnMsg.textContent = `Red wins!`
+
+    } else if (winner === -1) {
+      playerTurnMsg.textContent = `Black wins!`
+
+    }
+    gameBoardEl.removeEventListener('click', )
+
   } else {
-    render(); 
+    render();
   }
 
 }
@@ -225,7 +225,7 @@ function checkForWinner() {
 
 
 function getAllPlayerPucks(player) {
-  playerPucks = []; 
+  playerPucks = [];
   gameBoard.forEach((row, rowIdx) => {
     row.forEach((squareValue, colIdx) => {
       if (squareValue === player) {
@@ -237,7 +237,7 @@ function getAllPlayerPucks(player) {
 }
 
 function getAllPlayerKings(player) {
-  playerKings = []; 
+  playerKings = [];
   gameBoard.forEach((row, rowIdx) => {
     row.forEach((squareValue, colIdx) => {
       if (squareValue === player * 2) {
@@ -395,48 +395,51 @@ function checkForMoves(player) {
     }
   })
 
-  function canItMove(row, col) {
-    // debugger; 
-    if (row < 0 || col < 0 || row > 7 || col > 7 || gameBoard[row][col] === null) {
-      return false;
-    } else if (gameBoard[row][col] === 0) {
-      return true;
-    }
-  }
-
-  // Checking for jump logic
-  // first, is an opponent occupying an adjacent square 
-  function checkForOpponent(row, col) {
-    if (row < 0 || col < 0 || row > 7 || col > 7 || gameBoard[row][col] === null) {
-      return false;
-    } else if (gameBoard[row][col] === playerTurn * -1 || gameBoard[row][col] === playerTurn * -2) {
-      return true;
-    }
-  }
-
-  // second, if checkForOppoent is true, let's check the next space to see if it is open 
-  function canItJump(row, col) {
-    if (row < 0 || col < 0 || row > 7 || col > 7 || gameBoard[row][col] === null) {
-      return false;
-    } else if (gameBoard[row][col] === 0) {
-      return true;
-    }
-  }
   // if jumps are possible, don't return Moves. Jumps only. 
-  if (possibleJumps.length > 0) {
+  if (multiJumps.length > 0) {
+    movablePucks = [];
+    jumpsAvailable = [];
+  } else if (possibleJumps.length > 0) {
     possibleMoves = []
     jumpsAvailable = possibleMoves.concat(possibleJumps);
-    movablePucks = []; 
+    movablePucks = [];
   } else if (possibleMoves.length > 0) {
     movablePucks = possibleMoves
   }
 
-  // console.log(movablePucks)
   highlightMovablePucks();
-  return movablePucks, jumpsAvailable;
+  return movablePucks, jumpsAvailable, multiJumps;
 }
 
-function highlightMovablePucks(puck, row, col) {
+function canItMove(row, col) {
+  // debugger; 
+  if (row < 0 || col < 0 || row > 7 || col > 7 || gameBoard[row][col] === null) {
+    return false;
+  } else if (gameBoard[row][col] === 0) {
+    return true;
+  }
+}
+
+// Checking for jump logic
+// first, is an opponent occupying an adjacent square 
+function checkForOpponent(row, col) {
+  if (row < 0 || col < 0 || row > 7 || col > 7 || gameBoard[row][col] === null) {
+    return false;
+  } else if (gameBoard[row][col] === playerTurn * -1 || gameBoard[row][col] === playerTurn * -2) {
+    return true;
+  }
+}
+
+// second, if checkForOppoent is true, let's check the next space to see if it is open 
+function canItJump(row, col) {
+  if (row < 0 || col < 0 || row > 7 || col > 7 || gameBoard[row][col] === null) {
+    return false;
+  } else if (gameBoard[row][col] === 0) {
+    return true;
+  }
+}
+
+function highlightMovablePucks() {
   if (movablePucks.length > 0) {
     movablePucks.forEach((puck) => {
       const row = puck.currentRow;
@@ -453,6 +456,15 @@ function highlightMovablePucks(puck, row, col) {
       const puckSquare = rowEls[row].querySelectorAll('.square')[col]
       puckSquare.classList.add('movable-puck');
     })
+  } else if (multiJumps.length > 0) {
+    multiJumps.forEach((jump) => {
+      const row = jump.currentRow;
+      const col = jump.currentCol;
+
+      const puckSquare = rowEls[row].querySelectorAll('.square')[col]
+      puckSquare.classList.add('movable-puck');
+    })
+    // console.log('its pizza time')
   }
 }
 
@@ -472,7 +484,6 @@ function highlightDestination(row, col) {
         destinations.push({ newRow: movablePucks[i].newRow, newCol: movablePucks[i].newCol })
       }
     }
-    // console.log(destinations)
 
     destinations.forEach((destination) => {
       const row = destination.newRow;
@@ -495,7 +506,6 @@ function highlightDestination(row, col) {
         destinations.push({ currentRow: jumpsAvailable[i].currentRow, currentCol: jumpsAvailable[i].currentCol, newRow: jumpsAvailable[i].newRow, newCol: jumpsAvailable[i].newCol, removePuckFromRow: jumpsAvailable[i].removePuckFromRow, removePuckFromCol: jumpsAvailable[i].removePuckFromCol })
       }
     }
-    // console.log(destinations)
 
     destinations.forEach((destination) => {
       const row = destination.newRow;
@@ -508,40 +518,32 @@ function highlightDestination(row, col) {
       jumpedPuck.classList.add(`possible-jumped-puck`)
 
     })
+  } else if (multiJumps.length > 0) {
+    multiJumps.forEach((jump) => {
+      const row = jump.currentRow;
+      const col = jump.currentCol;
+
+      const puckSquare = rowEls[row].querySelectorAll('.square')[col];
+      puckSquare.classList.remove('movable-puck');
+    })
+
+    for (let i = 0; i < multiJumps.length; i++) {
+      if (row === multiJumps[i].currentRow && col === multiJumps[i].currentCol) {
+        destinations.push({ currentRow: multiJumps[i].currentRow, currentCol: multiJumps[i].currentCol, newRow: multiJumps[i].newRow, newCol: multiJumps[i].newCol, removePuckFromRow: multiJumps[i].removePuckFromRow, removePuckFromCol: multiJumps[i].removePuckFromCol })
+      }
+    }
+
+    destinations.forEach((destination) => {
+      const row = destination.newRow;
+      const col = destination.newCol;
+      const jumpedRow = destination.removePuckFromRow;
+      const jumpedCol = destination.removePuckFromCol;
+      const possibleDestination = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+      const jumpedPuck = document.querySelector(`[data-row="${jumpedRow}"][data-col="${jumpedCol}"]`)
+      possibleDestination.classList.add(`destination`)
+      jumpedPuck.classList.add(`possible-jumped-puck`)
+    })
   }
-
-  // return destinations; 
-  // square.classList.add('destination')
-
-  // CHECK FOR MOVES
-  // 1. check for kings 
-  // 2. ** check for possible jumps - force the player into that piece(s)
-  //    If multiple jumps, present those pieces as options. 
-  // 3. if no jumps, check for movable pieces
-  // 4. possible color change to denote movable pieces? 
-  // 5. ** possible hover state on movable pieces? 
-  // 
-  // SELECT AND MOVE PIECE 
-  // 1. Receive results of CHECK FOR MOVES
-  // 2. Select a piece - activated by event listener
-  // 3. click to select the piece to be moved 
-  // 4. display spaces the piece can be moved
-  // 5. move piece
-  // 
-  // Player Move options:
-  // 1. SELECT a space to move to by clicking on it 
-  // 2. DESELECT a piece by clicking anywhere else. ** should this message be displayed. or maybe a "cancel move button"
-  //
-  //
-  // CHECK AFTER MOVE 
-  // 1. Check for win
-  // 2. Check for king
-  // 3. Handle captured pieces 
-  // 4. Update player turn 
-
-  // make sure to invoke the neessary functions: 
-  // checkForWinner();
-  // changePlayer();
 }
 
 function clearDestinationHighlights() {
@@ -566,24 +568,96 @@ function movePuck(selectedPuck, oldRow, oldCol, newRow, newCol) {
   gameBoard[oldRow][oldCol] = 0;
 }
 
-function jumpPuck(selectedPuck, oldRow, oldCol, newRow, newCol, jumpedRow, jumpedCol) {
-  // Update the gameBoard to mark the jumped opponent's location as empty
-  gameBoard[jumpedRow][jumpedCol] = 0;
-
-  // Get the destination square where you want to move the puck
+function jumpPuck(selectedPuck, oldRow, oldCol, newRow, newCol) {
+  // multiJumps = []; 
+  const jumpedRow = (oldRow + newRow) / 2;
+  const jumpedCol = (oldCol + newCol) / 2;
   const destinationSquare = document.querySelector(`[data-row="${newRow}"][data-col="${newCol}"]`);
 
-  // Get the square where the jumped opponent's puck is
   const jumpedSquare = document.querySelector(`[data-row="${jumpedRow}"][data-col="${jumpedCol}"]`);
-
-  // Remove the jumped opponent's puck visually
+  // debugger; 
   jumpedSquare.innerHTML = '';
 
-  // Move the puck to the destination square
   destinationSquare.appendChild(selectedPuck);
   const puckValue = gameBoard[oldRow][oldCol];
-  gameBoard[newRow][newCol] = puckValue; 
-  gameBoard[oldRow][oldCol] = 0; 
+  gameBoard[newRow][newCol] = puckValue;
+  gameBoard[oldRow][oldCol] = 0;
+  gameBoard[jumpedRow][jumpedCol] = 0;
+// debugger
+  checkForMultiJump(newRow, newCol);
+  // console.log(selectedPuck, newRow, newCol)
+}
+
+function checkForMultiJump(row, col) {
+  const puck = gameBoard[row][col]
+  multiJumps = [];
+  if (puck === 1) {
+    // check up left
+    if (checkForOpponent(row - 1, col - 1)) {
+      if (canItJump(row - 2, col - 2)) {
+        multiJumps.push({ currentRow: row, currentCol: col, newRow: row - 2, newCol: col - 2, removePuckFromRow: row - 1, removePuckFromCol: col - 1 });
+        render();
+      }
+    }
+    // check up right
+    if (checkForOpponent(row - 1, col + 1)) {
+      if (canItJump(row - 2, col + 2)) {
+        multiJumps.push({ currentRow: row, currentCol: col, newRow: row - 2, newCol: col + 2, removePuckFromRow: row - 1, removePuckFromCol: col + 1 });
+        render();
+      }
+    }
+
+  } else if (puck === -1) {
+    // debugger
+
+    // check down left
+    if (checkForOpponent(row + 1, col - 1)) {
+      if (canItJump(row + 2, col - 2)) {
+        multiJumps.push({ currentRow: row, currentCol: col, newRow: row + 2, newCol: col - 2, removePuckFromRow: row + 1, removePuckFromCol: col - 1 });
+        render();
+      }
+    }
+    // check down right 
+    if (checkForOpponent(row + 1, col + 1)) {
+      if (canItJump(row + 2, col + 2)) {
+        multiJumps.push({ currentRow: row, currentCol: col, newRow: row + 2, newCol: col + 2, removePuckFromRow: row + 1, removePuckFromCol: col + 1 });
+        render();
+      }
+    }
+  } else if (puck === 2 || puck === -2) {
+    // check up left
+    if (checkForOpponent(row - 1, col - 1)) {
+      if (canItJump(row - 2, col - 2)) {
+        multiJumps.push({ currentRow: row, currentCol: col, newRow: row - 2, newCol: col - 2, removePuckFromRow: row - 1, removePuckFromCol: col - 1 });
+        render();
+      }
+    }
+    // check up right
+    if (checkForOpponent(row - 1, col + 1)) {
+      if (canItJump(row - 2, col + 2)) {
+        multiJumps.push({ currentRow: row, currentCol: col, newRow: row - 2, newCol: col + 2, removePuckFromRow: row - 1, removePuckFromCol: col + 1 });
+        render();
+      }
+    }
+    // check down left
+    if (checkForOpponent(row + 1, col - 1)) {
+      if (canItJump(row + 2, col - 2)) {
+        multiJumps.push({ currentRow: row, currentCol: col, newRow: row + 2, newCol: col - 2, removePuckFromRow: row + 1, removePuckFromCol: col - 1 });
+        render();
+      }
+    }
+    // check down right 
+    if (checkForOpponent(row + 1, col + 1)) {
+      if (canItJump(row + 2, col + 2)) {
+        multiJumps.push({ currentRow: row, currentCol: col, newRow: row + 2, newCol: col + 2, removePuckFromRow: row + 1, removePuckFromCol: col + 1 });
+        render();
+      }
+    }
+  }
+  
+  if (multiJumps.length === 0) {
+    changePlayer(); 
+  }
 }
 
 
@@ -592,13 +666,13 @@ function checkForKings() {
   // apply visual update 
   for (let i = 0; i < gameBoard[0].length; i++) {
     if (gameBoard[0][i] === 1) {
-      gameBoard[0][i] = 2; 
+      gameBoard[0][i] = 2;
     }
   }
 
-  for (let i = 0; i < gameBoard[7].length; i++){
+  for (let i = 0; i < gameBoard[7].length; i++) {
     if (gameBoard[7][i] === -1) {
-      gameBoard[7][i] = -2; 
+      gameBoard[7][i] = -2;
     }
   }
 }
@@ -611,29 +685,3 @@ function initialize() {
 
 
 initialize();
-
-
-
-// SAVE FOR LATER
-
-// const totalPucks = function (pucks, kings){
-//   return pucks.length + kings.length; 
-// }
-// console.log(totalPucks(getAllPlayerPucks(playerTurn), getAllPlayerKings(playerTurn)))
-
-
-// gameBoardEl.addEventListener('click', function (evt) {
-//   const clickedDestination = evt.target;
-//   if (clickedDestination.classList.contains('destination')) {
-//     console.log("please work")
-//   } else {
-//     destinations.forEach((destination) => {
-//       const row = destination.newRow;
-//       const col = destination.newCol;
-//       const possibleDestination = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
-//       possibleDestination.classList.remove(`destination`)
-//     })
-//     render();
-
-//   }
-// })
